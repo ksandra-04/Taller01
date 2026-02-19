dc7ba260b45268f6878606ede1161cb183d2b651
ksandra-04 <kasandrapimienta1118030@correo.itm.edu.co>
2026-02-18 20:35:22 -0500

    Add basic Time class structure with fields and empty constructor

---
+++ Taller01/Taller01/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller01
+{
+    public class Time
+    {
+        private int _hour;
+        private int _minute;
+        private int _second;
+        private int _millisecond;
+
+        public Time()
+        {
+        }
+    }
+}
+
