12b278e570a7c54c790c67c25e5b0294c66cc0c3
ksandra-04 <kasandrapimienta1118030@correo.itm.edu.co>
2026-02-18 20:49:18 -0500

    Builders are added

--- Taller01/Taller01/Time.cs
+++ Taller01/Taller01/Time.cs
@@ -6,14 +6,42 @@ namespace Taller01
 {
     public class Time
     {
-        private int _hour;
-        private int _minute;
-        private int _second;
-        private int _millisecond;
-
-        public Time()
+        private int hour;
+        private int minute;
+        private int second;
+        private int millisecond;
+       
+        public Time() //sin parametros
         {
+            hour = 0;
+            minute = 0;
+            second = 0;
+            millisecond = 0;
         }
+        public Time(int hour)
+        {
+            this.hour = hour;
+            this.minute = 0;
+            this.second = 0;
+            this.millisecond = 0;
+        }
+        public Time(int hour, int minute)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = 0;
+            this.millisecond = 0;
+        }
+        public Time(int hour, int minute, int second, int millisecond)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+            this.millisecond = millisecond;
+        }
+
+
+
     }
 }
 
