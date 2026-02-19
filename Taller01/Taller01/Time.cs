f04fb18fe1e767ded985f23a6f98078a4bd14b4e
ksandra-04 <kasandrapimienta1118030@correo.itm.edu.co>
2026-02-18 20:54:34 -0500

    Adjustment and validation are performed on the main constructor

--- Taller01/Taller01/Time.cs
+++ Taller01/Taller01/Time.cs
@@ -18,27 +18,40 @@ namespace Taller01
             second = 0;
             millisecond = 0;
         }
+        // Solo hora
         public Time(int hour)
-        {
-            this.hour = hour;
-            this.minute = 0;
-            this.second = 0;
-            this.millisecond = 0;
-        }
+            : this(hour, 0, 0, 0) { }
+
+        // Hora y minuto
         public Time(int hour, int minute)
-        {
-            this.hour = hour;
-            this.minute = minute;
-            this.second = 0;
-            this.millisecond = 0;
-        }
+            : this(hour, minute, 0, 0) { }
+
+        // Hora, minuto y segundo
+        public Time(int hour, int minute, int second)
+            : this(hour, minute, second, 0) { }
+
+        // Constructor completo
         public Time(int hour, int minute, int second, int millisecond)
         {
+            if (hour < 0 || hour > 23)
+                throw new Exception($"The hour: {hour}, is not valid.");
+
+            if (minute < 0 || minute > 59)
+                throw new Exception($"The minute: {minute}, is not valid.");
+
+            if (second < 0 || second > 59)
+                throw new Exception($"The second: {second}, is not valid.");
+
+            if (millisecond < 0 || millisecond > 999)
+                throw new Exception($"The millisecond: {millisecond}, is not valid.");
+
             this.hour = hour;
             this.minute = minute;
             this.second = second;
             this.millisecond = millisecond;
         }
+
+
 
 
 
